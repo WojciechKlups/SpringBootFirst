4ccbcbbcfec455581140379e53b36f2f2cc837ba
Wojciech Klupś <wojciech.klups@gmail.com>
2019-06-15 15:46:41 +0200

    Added Book model and BookDao class

---
+++ src/main/java/pl/sda/springbootstart/dao/BookDao.java
@@ -0,0 +1,24 @@
+package pl.sda.springbootstart.dao;
+
+import org.springframework.stereotype.Repository;
+import pl.sda.springbootstart.model.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class BookDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void save(Book book){
+        entityManager.persist(book);
+    }
+
+    public Book get(Long id){
+        Book book = entityManager.find(Book.class, id);
+        return book;
+    }
+
+}
