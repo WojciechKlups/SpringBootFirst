07ba4e43fdf1680d51d5033c4d42996ab9086799
Wojciech Klupś <wojciech.klups@gmail.com>
2019-06-16 12:38:54 +0200

    Added querys

--- src/main/java/pl/sda/springbootstart/SpringBootStartApplication.java
+++ src/main/java/pl/sda/springbootstart/SpringBootStartApplication.java
@@ -6,6 +6,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 import pl.sda.springbootstart.dao.BookDao;
 import pl.sda.springbootstart.model.Book;
 import pl.sda.springbootstart.producers.MessageProducer;
+import pl.sda.springbootstart.repository.CarRepository;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringBootStartApplication {
@@ -15,39 +19,110 @@ public class SpringBootStartApplication {
 //		MessageProducer messageProducer = context.getBean(MessageProducer.class);
 //
 //		messageProducer.printMessage();
+//
+//        BookDao bookDao = context.getBean(BookDao.class);
+//
+//        Book book = new Book();
+//        book.setIsbn("197324913");
+//        book.setAuthor("Pablo Picasso");
+//        book.setTitle("Dao Zabrao");
+//        book.setPages(123);
+//
+//        bookDao.save(book);
+//
+//        Book book1 = bookDao.get(1L);
+//        System.out.println(book1);
+//
+//        Book book2 = new Book();
+//        book2.setIsbn("92384798");
+//        book2.setAuthor("Jan Kowalski");
+//        book2.setTitle("Abecadlo");
+//        book2.setPages(213);
+//
+//        bookDao.save(book2);
+//
+//        Book book3 = bookDao.get(8L);
+//        System.out.println(book3);
+//
+// //       bookDao.delete(2L);
+//
+//        book3.setAuthor("Grzegorz Nowak");
+//        book3.setTitle("Abecadlo 2.0");
+//        bookDao.update(book3);
+//
 
-        BookDao bookDao = context.getBean(BookDao.class);
+            CarRepository carRepository = context.getBean(CarRepository.class);
+            //wszystkie auta
+            carRepository.findAll().forEach(System.out::println);
 
-        Book book = new Book();
-        book.setIsbn("197324913");
-        book.setAuthor("Pablo Picasso");
-        book.setTitle("Dao Zabrao");
-        book.setPages(123);
+            System.out.println("==========================================");
 
-        bookDao.save(book);
+            //wszystkie auta marki Opel
+            carRepository.findAllByBrand("Opel").forEach(System.out::println);
 
-        Book book1 = bookDao.get(1L);
-        System.out.println(book1);
+            System.out.println("============================================");
 
-        Book book2 = new Book();
-        book2.setIsbn("92384798");
-        book2.setAuthor("Jan Kowalski");
-        book2.setTitle("Abecadlo");
-        book2.setPages(213);
+            //wszystkie auta z ceną pomiędzy xxx a xxx
+            carRepository.findAllByPriceBetween(15000.00, 19000.00).forEach(System.out::println);
+            System.out.println("============================================");
 
-        bookDao.save(book2);
+            //wszystkie auta danej marki i modelu
+            carRepository.findAllByBrandAndModel("Opel", "Astra").forEach(System.out::println);
+            System.out.println("============================================");
 
-        Book book3 = bookDao.get(2L);
-        System.out.println(book3);
+            //wszystkie auta z ceną wyższą od...
+            carRepository.findAllByPriceGreaterThan(21000.00).forEach(System.out::println);
+            System.out.println("============================================");
 
- //       bookDao.delete(2L);
+            //wszystkie auta koloru xxx
+            carRepository.findAllByColor("blue").forEach(System.out::println);
+            System.out.println("============================================");
 
-        book3.setAuthor("Grzegorz Nowak");
-        book3.setTitle("Abecadlo 2.0");
-        bookDao.update(book3);
+            //pierwszy samochód ze wskazaną ceną
+            carRepository.findFirstByPrice(35000.00).forEach(System.out::println);
+            System.out.println("============================================");
+
+            //pierwsze 3 samachody danej marki
+            carRepository.findFirst3ByBrand("Opel").forEach(System.out::println);
+            System.out.println("============================================");
+
+            //wszystkie samochody danej marki z podaną ceną
+            carRepository.findAllByBrandAndPrice("Ford", 19000.00).forEach(System.out::println);
+            System.out.println("============================================");
+
+            //wszystkie samochody posortowane po cenie
+            carRepository.findAllByOrderByPriceAsc().forEach(System.out::println);
+            System.out.println("============================================");
+
+            //wszystkie samochody dwóch podanych marek
+            carRepository.findAllByBrandOrBrand("Opel", "Skoda").forEach(System.out::println);
+            System.out.println("============================================");
+
+            //wszystkie samochody jednego modelu spośród 3 podanych
+            carRepository.findAllByModelIn(Arrays.asList("Astra", "Skoda", "Golf")).forEach(System.out::println);
+            System.out.println("============================================");
+            //wszystkie samochody z modelem zaczynającym sie na literę...
+            carRepository.findAllByModelStartsWith("Mu").forEach(System.out::println);
+            System.out.println("============================================");
+
+            //wszystkie samochody z marką kończącą się na "a"
+            carRepository.findAllByBrandEndsWith("el").forEach(System.out::println);
+            System.out.println("============================================");
+
+            //wszystkie samochody z marką zawierającą literę...
+            carRepository.findAllByBrandContains("pe").forEach(System.out::println);
+            System.out.println("============================================");
 
 
-        context.close();
+            //wszystkie samochody z marką zaczynającą się na literę ... oraz ceną większą niż ... posortowane po modelu rosnąco
+            carRepository.findCarByBrandAndPriceParams("O%", 16000.00);
+            System.out.println("============================================");
+
+            //wszystkie samochody z daną marką i modelem oraz ceną mniejszą niż... posortowane po cenie malejąco
+            carRepository.findCarByBrandAndModelAndPriceParams("Opel", "Astra", 16000.000);
+            System.out.println("============================================");
+            
+            context.close();
 
 	}
 
--- src/main/java/pl/sda/springbootstart/repository/CarRepository.java
+++ src/main/java/pl/sda/springbootstart/repository/CarRepository.java
@@ -1,9 +1,57 @@
 package pl.sda.springbootstart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.sda.springbootstart.model.Car;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
+
+    List<Car> findAllByBrand(String brand);
+
+    List<Car> findAllByPriceBetween(double price1, double price2);
+
+    List<Car> findAllByBrandAndModel(String brand, String model);
+
+    List<Car> findAllByPriceGreaterThan(double price);
+
+    List<Car> findAllByColor(String color);
+
+    List<Car> findFirstByPrice(double price);
+
+    List<Car> findFirst3ByBrand(String brand);
+
+    List<Car> findAllByBrandAndPrice(String brand, double price);
+
+    List<Car> findAllByOrderByPriceAsc();
+
+    List<Car> findAllByBrandOrBrand(String brand1, String brand2);
+
+    List<Car> findAllByModelIn(List<String> cars);
+
+    List<Car> findAllByModelStartsWith(String start);
+
+    List<Car> findAllByBrandEndsWith(String end);
+
+    List<Car> findAllByBrandContains(String contain);
+
+    @Query("select c from Car c where c.brand like :start and c.price > :price order by c.model asc")
+    Car findCarByBrandAndPriceParams(
+            @Param("start") String start,
+            @Param("price") Double price);
+
+    @Query("select c from Car c where c.brand like :brand and c.model like :model and c.price < :price order by c.price desc")
+    Car findCarByBrandAndModelAndPriceParams(
+            @Param("brand") String brand,
+            @Param("model") String model,
+            @Param("price") Double price);
+
 }
+
+
+
+
